f44dc7867aa46309e94f942861063164f71250da
Agus Puryanto <aguspuryanto@gmail.com>
2023-08-09 09:19:43 +0700

    first commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,15 +14,23 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('last_name');
+            $table->increments('id');
+            $table->string('full_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        // Insert some stuff
+        DB::table('users')->insert(
+            array(
+                'full_name' => 'Administrator',
+                'email' => 'adminlaundry@gmail.com',
+                'password' => bcrypt('admin1234')
+            )
+        );
     }
 
     /**
